7285db5277fc53505418061bc75e4bf2baeaccae
MD ASIF JAWED <110151368+mdasifj625@users.noreply.github.com>
2023-07-24 21:27:47 +0530

    Update get-all.js

--- src/modules/appointment/controller/get-all.js
+++ src/modules/appointment/controller/get-all.js
@@ -1,7 +1,7 @@
 export default async function _GetAll(req, res) {
   try {
     // Get All Appointment Document
-    const appointments = await App.Models.Appointment.find()
+    const appointments = await App.Models.Appointment.find().sort({ createdAt:-1 })
 
     return res.success({
       message: App.Messages.Appointment.Success.GetAll,
