a765d3806a9193db9169e4881d52fd2aed74489a
mdasifj625 <mdasifj625@gmail.com>
2024-09-04 10:04:56 +0530

    removed unwanted api call

--- src/app.routes.js
+++ src/app.routes.js
@@ -1,19 +1,10 @@
 import { Router } from 'express'
 import { appointmentRouter } from './modules/appointment/routers/index.js'
-import axios from 'axios'
 
 const router = Router()
 
 router.get('/ping', (req, res) => {
-  axios
-    .get(
-      'https://gamerouter.spartanpoker.com/ping'
-    )
-    .then((prod_ping) => {
-      console.log('Production ping response: ', prod_ping.data)
-      res.send(prod_ping.data)
-    })
-  // res.send('success')
+  res.send('success')
 })
 
 router.use('/appointments', appointmentRouter)
